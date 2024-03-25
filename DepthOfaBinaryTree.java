1b0f382def51002cc0f70e3f6f03b4b716477fb2
jain.monisha <61319556+jainmonisha17@users.noreply.github.com>
2024-03-25 21:33:17 +0530

    Create DepthOfaBinaryTree.java

---
+++ DepthOfaBinaryTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int level = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode temp = queue.remove();
+                if(temp.left != null) {
+                    queue.add(temp.left);
+                }
+                if(temp.right != null) {
+                    queue.add(temp.right);
+                }
+            }
+        level++;
+        }
+        return level;
+    }
+}

65b01130fe41f42fff65931e2e119a04eb0379a3
jain.monisha <61319556+jainmonisha17@users.noreply.github.com>
2024-06-28 22:57:09 +0530

    Add files via upload

---
+++ RotateArrByKSteps.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class RotateArrByKSteps {
+    public static void solve(int[] arr, int k) {
+        k = k % arr.length;
+        reverse(arr, 0, arr.length - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k + 1, arr.length - 1);
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
